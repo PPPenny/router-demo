64c02ab1533f8df64501a7b7347d4ffe0b54bd59
PPPenny <1020344938@qq.com>
2018-01-17 11:27:31 +0800

    完善router
    完善router

--- src/component/footer/footer.js
+++ src/component/footer/footer.js
@@ -1,6 +1,6 @@
 import React,{Component}from 'react';
 import FooterItem from './footer-item';
-import { BrowserRouter, IndexRoute, Route, Link } from "react-router-dom";
+import { BrowserRouter, Route, Link } from "react-router-dom";
 import OrdersIndex from '../pages/orders/index';
 import OrderIndex from '../pages/order/index';
 import ClueIndex from '../pages/clue/index';
@@ -11,26 +11,28 @@ class Footer extends Component{
     render(){
         return(
             <div>
-            <div className="footDiv">
-                <Link to="/pages/orders"><FooterItem txt="订单"/></Link>
-                <Link to="/pages/order"><FooterItem txt="下单"/></Link>
-                <Link to="/pages/clue"><FooterItem txt="线索"/></Link>
-                <Link to="/pages/my"><FooterItem txt="我"/></Link>
-            </div>
-            <main>
-                <IndexRoute component={OrdersIndex}/>
-                <Route path="/pages/orders" exact component={OrdersIndex} />
-                <Route path="/pages/order" exact component={OrderIndex} />
-                <Route path="/pages/clue" exact component={ClueIndex} />
-                <Route path="/pages/my" exact component={MyIndex} />
-           </main>
+                <div className="footDiv">
+                    <Link to="/pages/orders"><FooterItem txt="订单"/></Link>
+                    <Link to="/pages/order"><FooterItem txt="下单"/></Link>
+                    <Link to="/pages/clue"><FooterItem txt="线索"/></Link>
+                    <Link to="/pages/my"><FooterItem txt="我"/></Link>
+                </div>
+                <main>
+                    <Route path="/" exact component={OrdersIndex}/>
+                     
+                        <Route path="/pages/orders" exact component={OrdersIndex} />
+                        <Route path="/pages/order" exact component={OrderIndex} />
+                        <Route path="/pages/clue" exact component={ClueIndex} />
+                        <Route path="/pages/my" exact component={MyIndex} />
+                    
+               </main>
             </div>
         )
     }
 }
-const footDom = ()=>{
+const FootDom = ()=>
     <BrowserRouter>
       <Footer/>
     </BrowserRouter>
-}
-export default footDom;
+
+export default FootDom;
--- src/index.js
+++ src/index.js
@@ -1,8 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import footDom from '../src/component/footer/footer';
+import FootDom from '../src/component/footer/footer';
 import registerServiceWorker from './registerServiceWorker';
 
-ReactDOM.render(<footDom />, document.getElementById('root'));
+ReactDOM.render(<FootDom />, document.getElementById('root'));
 registerServiceWorker();
