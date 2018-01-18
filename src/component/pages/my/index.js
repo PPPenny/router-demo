10040f61119d9f417bffb1ee95adecd73efd63a4
PPPenny <1020344938@qq.com>
2018-01-18 18:56:50 +0800

    完善
    完善

--- src/component/pages/my/index.js
+++ src/component/pages/my/index.js
@@ -1,7 +1,42 @@
 import React,{Component}from 'react';
+import axios from 'axios';
+import {PersonInfo,ServerDom} from './myIndex';
+import './index.css';
+import myInfoData from './json/myInfo.json'
  class MyIndex extends Component{
+    constructor(){
+        super();
+         this.dataSourse = {};
+        this.get = this.get.bind(this);
+    }
+    componentWillMount(){
+        this.get();
+    }
+    get(){
+        // axios.get('./json/myInfo.json').then((data)=>{
+        //     if(data.result === "0"){
+                window.localStorage['myInfo'] = JSON.stringify(myInfoData.data);
+        //         this.dataSourse = data.data;
+        //     }
+        //     }).catch((error)=>{
+        //         console.log("出错了");
+
+        //     })
+
+        }
+    
      render(){
-         return(<div>个人中心首页</div>);
+         return(<div>
+             <PersonInfo dataValue={myInfoData.data}/>
+             <div className="goWhere">
+                 <ul className="goOther">
+                    <ServerDom dataType="service" txt="联系客服" iconName="icon-arrow-down"/>
+                    <ServerDom dataType="feedback" txt="问题反馈" iconName="icon-arrow-right"/>
+                    <ServerDom dataType="notice" txt="公告" iconName="icon-arrow-right"/>
+                    <ServerDom dataType="agent-about" txt="关于安惠通" iconName="icon-arrow-right"/>
+                 </ul>
+             </div>
+         </div>);
      }
  }
  export default MyIndex;
---
+++ src/component/pages/my/myIndex.js
@@ -0,0 +1,34 @@
+import React,{Component}from 'react';
+import './index.css'
+class PersonInfo extends Component{
+    render(){
+        return(<div>
+                    <img className="user-head" src="./imgs/defult-head.png" alt="用户默认头像" />
+                    <div id="user-info">
+                        <div className="messageInfo">
+                            <div className="user-name nowrap">{this.props.dataValue.name}</div>
+                        </div>
+                        <div className="user-addr nowrap">{this.props.dataValue.dealerName}</div>
+                    </div>
+                    <i className="icon-arrow-right icon-arrow-head"></i>
+            </div>
+            )   
+         }
+}
+
+class ServerDom extends Component{
+    render(){
+        return(
+            <li className={this.props.dataType}>        
+            <span className="border-b">{this.props.txt}</span>
+            <i className={this.props.iconName}></i>
+        </li>
+        )
+         
+    }
+   
+}
+export {PersonInfo,ServerDom}
+
+
+
